1de9d9f9a338df639227c7fbe09f70b722e3f079
Dhassan <dimitri16h@gmail.com>
2024-11-08 18:31:44 -0500

    Day 3

--- TodoListPython/main.py
+++ TodoListPython/main.py
@@ -1,8 +1,17 @@
-user_prompt = "Enter todo: "
+todos = ["run", "jog"]
 
-todos = []
+while True:
+    user_action = input("Type add, show or exit: ")
+    user_action = user_action.strip()
 
-while todo != "":
-    todo = input(user_prompt)
-    print(todo.capitalize())
-    todos.append(todo)
+    # using if statements as switch statements not in Python 3.6
+    if (user_action == 'add'): 
+        todo = input("Enter a todo: ")
+        todos.append(todo)
+    elif (user_action == 'show'):
+        for item in todos:
+            print(item)
+    elif (user_action == 'exit'):
+        break
+
+print("Bye!")
