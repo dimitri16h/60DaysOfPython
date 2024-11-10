e596d8620846bf38c88354290e254f0874af3781
Dhassan <dimitri16h@gmail.com>
2024-11-10 14:16:54 -0500

    Day 5

--- TodoListPython/main.py
+++ TodoListPython/main.py
@@ -1,21 +1,30 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit, or exit: ")
+    user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
     # using if statements as switch statements not in Python 3.6
     if (user_action == 'add'): 
         todo = input("Enter a todo: ")
         todos.append(todo)
+
     elif (user_action == 'show'):
-        for item in todos:
-            print(item)
-    elif (user_actions == 'edit'):
-        number = input("Number of the todo to edit: ")
-        existing_todo = todos[number-1]
+        for index, item in enumerate(todos):
+            row = f"{index+1}-{item}"
+            print(row)
+
+    elif (user_action == 'edit'):
+        number = int(input("Number of the todo to edit: "))
+        number -= 1
+        existing_todo = todos[number]
         new_todo = input("Enter new todo: ")
         todos[number] = new_todo
+
+    elif (user_action == "complete"):
+        number = int(input("Number of the todo to complete: "))
+        todos.pop(number-1)
+
     elif (user_action == 'exit'):
         break
 
